c2319fae88865712fc3b385895dff44124b037c6
Nathan Zheng <nyz3@cornell.edu>
2017-12-09 03:51:53 -0500

    added basic code for opening excel files and streams

--- src/excelreadwrite/Main.java
+++ src/excelreadwrite/Main.java
@@ -1,5 +1,36 @@
 package excelreadwrite;
 
-public class Main {
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		
+		//inputstream "fileIn" is the file we are reading data/info from
+		FileInputStream fileIn = new FileInputStream(new File("FinancialData.xlsx"));
+		//outputstream "fileOut" is the file we are writing out manipulated data to
+		FileOutputStream fileOut = new FileOutputStream(new File("FinancialDataAnalysis.xlsx"));
+		
+		//create workbook from the given excel file to allow access to cells/data, e.g getNumberofSheets()
+		XSSFWorkbook workbook = new XSSFWorkbook(fileIn);
+		
+		//gets first sheet of the excel file, edit later for numerous sheets
+		XSSFSheet sheet = workbook.getSheetAt(0); 
+		
+		
+		workbook.close();
+		fileOut.close();
+		fileIn.close();
+		
+		
+		
+		
+		
+	}
 }
